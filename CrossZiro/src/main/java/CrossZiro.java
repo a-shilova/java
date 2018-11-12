d1865b7b6de17563b2b867bbd315c9aeb2c5116f
a-shilova <32563685+a-shilova@users.noreply.github.com>
2018-11-12 23:29:48 +0500

    Refactoring

--- CrossZiro/src/main/java/CrossZiro.java
+++ CrossZiro/src/main/java/CrossZiro.java
@@ -1,23 +1,21 @@
-import java.util.Random;
 import java.util.Scanner;
 
 public class CrossZiro {
-	private static int SIZE;
-	private static int DOTS_TO_WIN;
+	private static int size;
+	private static int dotsToWin;
 	private static final char DOT_EMPTY = '•';
 	private static final char DOT_X = 'X';
 	private static final char DOT_O = 'O';
 	private static char[][] map;
 	private static Scanner sc = new Scanner(System.in);
-	private static Random rand = new Random();
 
 	public static void main(String[] args) {
 		initMap();
 		printMap();
 		while (true) {
-			humanTurn();
+			int[] humanTurnCord = humanTurn();
 			printMap();
-			if (checkWin(DOT_X)) {
+			if (checkWin(humanTurnCord)) {
 				System.out.println("Вы победили!");
 				break;
 			}
@@ -25,9 +23,9 @@ public class CrossZiro {
 				System.out.println("Ничья");
 				break;
 			}
-			aiTurn();
+			int[] aiTurnCord = aiTurn(humanTurnCord);
 			printMap();
-			if (checkWin(DOT_O)) {
+			if (checkWin(aiTurnCord)) {
 				System.out.println("Победил Компьютер");
 				break;
 			}
@@ -39,168 +37,222 @@ public class CrossZiro {
 		System.out.println("Игра закончена");
 	}
 
-	private static boolean checkWin(char symb) {
-		for (int i = 0; i < SIZE; i++ ) {
-			int equalsVertical = 0;
-			int equalsGorizontal = 0;
-			int equalsR = 0;
-			int equalsL = 0;
+	private static boolean checkWin(int[] turn) {
+		int i = turn[1];
+		int j = turn[0];
+		char symb = map[i][j];
 
-			for (int j = 0; j < SIZE; j++) {
-				boolean isEqualsVertical = symb == map[i][j];
-				if (isEqualsVertical) equalsVertical += 1;
+		boolean isWinInGorizontal = checkGorizontal(i, j, symb);
+		boolean isWinInVertical = checkVertical(i, j, symb);
+		boolean isWinToLeftDiagonal = checkLeftDiagonal(i, j, symb);
+		boolean isWinToRightDiagonal = checkRightDiagonal(i, j, symb);
 
-				boolean equalsGorizontal = symb == map[j][i];
-				if (equalsGorizontal) equalsGorizontal += 1;
+		return isWinInGorizontal || isWinInVertical || isWinToLeftDiagonal || isWinToRightDiagonal;
+	}
 
-				boolean isLeftDiagonal = i == j;
-				boolean isRightDiagonal = SIZE - i - 1 == j;
-				if (isEqualsVertical && isLeftDiagonal) equalsL += 1;
-				if (isEqualsVertical && isRightDiagonal) equalsR += 1;
+	private static boolean checkGorizontal(int y, int x, char symb) {
+		int count = 0;
+
+		for (int i = x; i >= 0; i--) {
+			if (map[y][i] == symb) {
+				count++;
+			} else {
+				break;
 			}
-
-			if (isEqualsVertical == DOTS_TO_WIN) return true;
-			if (isEqualsGorizontal == DOTS_TO_WIN) return true;
-			if (equalsR == DOTS_TO_WIN || equalsL == DOTS_TO_WIN) return true;
 		}
 
-		return false;
+		for (int i = x; i < size; i++) {
+			if (map[y][i] == symb) {
+				count++;
+			} else {
+				break;
+			}
+		}
+
+		return count - 1 == dotsToWin;
+	}
+
+	private static boolean checkVertical(int y, int x, char symb) {
+		int count = 0;
+
+		for (int i = y; i >= 0; i--) {
+			if (map[i][x] == symb) {
+				count++;
+			} else {
+				break;
+			}
+		}
+
+		for (int i = y; i < size; i++) {
+			if (map[i][x] == symb) {
+				count++;
+			} else {
+				break;
+			}
+		}
+
+		return count - 1 == dotsToWin;
+	}
+
+	private static boolean checkRightDiagonal(int y, int x, char symb) {
+		int count = 0;
+
+		for (int i = y, j = x; i < size && j >= 0; i++, j--) {
+			if (map[i][j] == symb) {
+				count++;
+			} else {
+				break;
+			}
+		}
+
+		for (int i = y, j = x; i >= 0 && j < size; i--, j++) {
+			if (map[i][j] == symb) {
+				count++;
+			} else {
+				break;
+			}
+		}
+
+		return count - 1 == dotsToWin;
+	}
+
+	private static boolean checkLeftDiagonal(int y, int x, char symb) {
+		int count = 0;
+
+		for (int i = y, j = x; i < size && j < size; i++, j++) {
+			if (map[i][j] == symb) {
+				count++;
+			} else {
+				break;
+			}
+		}
+
+		for (int i = y, j = x; i >= 0 && j >= 0; i--, j--) {
+			if (map[i][j] == symb) {
+				count++;
+			} else {
+				break;
+			}
+		}
+
+		return count - 1 == dotsToWin;
 	}
 
 	private static boolean isMapFull() {
-		for (int i = 0; i < SIZE; i++) {
-			for (int j = 0; j < SIZE; j++) {
+		for (int i = 0; i < size; i++) {
+			for (int j = 0; j < size; j++) {
 				if (map[i][j] == DOT_EMPTY) return false;
 			}
 		}
 		return true;
 	}
 
-	private static aiTurn() {
-		int x, y;
-		for (int i = 0; i < SIZE; i++) {
-			for (int j = 0; j < SIZE; j++) {
-				if (map[i][j] == DOT_O) {
-					if (i - 1 >= 0) {
-						char up = map[i - 1][j];
-						if (up == DOT_EMPTY) {
-							x = j;
-							y = i - 1;
-							break;
-						}
+	private static int[] aiTurn(int[] humanTurn) {
+		int x = -1;
+		int y = -1;
+		int i = humanTurn[0];
+		int j = humanTurn[1];
+		boolean isDone = false;
 
-						if (j - 1 >= 0) {
-							char leftUpD = map[i - 1][j - 1];
-							if (leftUpD == DOT_EMPTY) {
-								x = j - 1;
-								y = i - 1;
-								break;
-							}
-						}
-
-						if (j + 1 < SIZE) {
-							char rightUpD = map[i - 1][j + 1];
-							if (rightUpD == DOT_EMPTY) {
-								x = j + 1;
-								y = i - 1;
-								break;
-							}
-						}
-					}
-
-					if (i + 1 < SIZE) {
-						char down = map[i + 1][j];
-						if (down == DOT_EMPTY) {
-							x = j;
-							y = i + 1;
-							break;
-						}
-
-						if (j - 1 >= 0) {
-							char leftDownD = map[i + 1][j - 1];
-							if (leftDownD == DOT_EMPTY) {
-								x = j - 1;
-								y = i + 1;
-								break;
-							}
-						}
-
-						if (j + 1 < SIZE) {
-							char rightDownD = map[i + 1][j + 1];
-							if (rightDownD == DOT_EMPTY) {
-								x = j + 1;
-								y = i + 1;
-								break;
-							}
-						}
-					}
-
-					if (j - 1 >= 0) {
-						char left = map[i][j - 1];
-						if (left == DOT_EMPTY) {
-							x = j - 1;
-							y = i;
-							break;
-						}
-					}
-
-					if (j + 1 < SIZE) {
-						char right = map[i][j + 1];
-						if (right == DOT_EMPTY) {
-							x = j + 1;
-							y = i;
-							break;
-						}
-					}
-				}
-			}
+		if (!isDone && isCellValid(i - 1, j)) {
+			y = j;
+			x = i - 1;
+			isDone = true;
 		}
+
+		if (!isDone && isCellValid(i - 1, j - 1)) {
+			y = j - 1;
+			x = i - 1;
+			isDone = true;
+		}
+
+		if (!isDone && isCellValid( i - 1, j + 1)) {
+			y = j + 1;
+			x = i - 1;
+			isDone = true;
+		}
+
+		if (!isDone && isCellValid(i + 1, j)) {
+			y = j;
+			x = i + 1;
+			isDone = true;
+		}
+
+		if (!isDone && isCellValid(i + 1, j - 1)) {
+			y = j - 1;
+			x = i + 1;
+			isDone = true;
+		}
+
+		if (!isDone && isCellValid( i + 1, j + 1)) {
+			y = j + 1;
+			x = i + 1;
+			isDone = true;
+		}
+
+		if (!isDone && isCellValid( i, j - 1)) {
+			y = j - 1;
+			x = i;
+			isDone = true;
+		}
+
+		if (!isDone && isCellValid(i, j + 1)) {
+			y = j + 1;
+			x = i;
+			isDone = true;
+		}
+
 		System.out.println("Компьютер сделал ход в точку " + (x + 1) + " " + (y + 1));
 		map[y][x] = DOT_O;
+		return new int[]{x, y};
 	}
 
-	private static humanTurn() {
+	private static int[] humanTurn() {
 		int x, y;
+
 		do {
 			System.out.println("Введите координаты в формате X Y");
 			x = sc.nextInt() - 1;
 			y = sc.nextInt() - 1;
 		} while (!isCellValid(x, y));
 		map[y][x] = DOT_X;
+
+		return new int[]{x, y};
 	}
 
 	private static boolean isCellValid(int x, int y) {
-		if (x < 0 || x >= SIZE || y < 0 || y >= SIZE) return false;
-		if (map[y][x] == DOT_EMPTY) return true;
+		boolean isInSize = x >= 0 && x < size && y >= 0 && y < size;
+		if (isInSize) return map[y][x] == DOT_EMPTY;
+
 		return false;
 	}
 
 	private static void initMap() {
 		System.out.println("Введите размер поля (число от 3 до 5)");
-		SIZE = sc.nextInt();
-		if (SIZE < 3 || SIZE > 5) {
+		size = sc.nextInt();
+		if (size < 3 || size > 5) {
 			System.out.println("Неверный размер поля");
 			initMap();
 		} else {
-			map = new char[SIZE][SIZE];
-			for (int i = 0; i < SIZE; i++) {
-				for (int j = 0; j < SIZE; j++) {
+			map = new char[size][size];
+			for (int i = 0; i < size; i++) {
+				for (int j = 0; j < size; j++) {
 					map[i][j] = DOT_EMPTY;
 				}
 			}
-			if (SIZE == 3) DOTS_TO_WIN = 3;
-			if (SIZE > 3) DOTS_TO_WIN = 4;
+			if (size == 3) dotsToWin = 3;
+			if (size > 3) dotsToWin = 4;
 		}
 	}
 
 	private static void printMap() {
-		for (int i = 0; i <= SIZE; i++) {
+		for (int i = 0; i <= size; i++) {
 			System.out.print(i + " ");
 		}
 		System.out.println();
-		for (int i = 0; i < SIZE; i++) {
+		for (int i = 0; i < size; i++) {
 			System.out.print((i + 1) + " ");
-			for (int j = 0; j < SIZE; j++) {
+			for (int j = 0; j < size; j++) {
 				System.out.print(map[i][j] + " ");
 			}
 			System.out.println();
